dff0d749bcd171878a82b60090ac0fc1bbbe38cd
Aziendale <Aziendale@jNoteBook.homenet.telecomitalia.it>
2018-03-22 17:56:13 +0100

    il try lo fa sul servizio e non piu' sul bat

--- src/main/java/it/winetsolutions/sqserverchecker/Main.java
+++ src/main/java/it/winetsolutions/sqserverchecker/Main.java
@@ -1,8 +1,10 @@
 package it.winetsolutions.sqserverchecker;
 
 import java.io.IOException;
+import java.io.StringWriter;
 import java.net.Socket;
 import org.apache.commons.exec.ExecuteException;
+import org.apache.commons.io.IOUtils;
 
 
 public class Main {
@@ -23,15 +25,31 @@ public class Main {
 	public static void main(String[] args) {
 		
 		if (!serverListening(HOST,PORT)) {
-			String path = "C:/sonarqube-6.7.2/bin/windows-x86-64/StartSonar.bat";
-//			CommandLine commandLine = CommandLine.parse(path);
-//			DefaultExecutor executor = new DefaultExecutor();
-//			executor.setExitValue(1);
-			try {
-				System.out.println("I'm trying to start "+path);
-				Runtime.getRuntime().exec("cmd /c start "+path);
-//				int exitValue = executor.execute(commandLine);
-//				System.out.println("Executed. Exit value: "+exitValue);
+			
+			try {    
+			    // start
+			    String[] script = {"cmd.exe", "/c", "sc", "start", "SonarQube"};
+			    // stop
+			    // String[] script = {"cmd.exe", "/c", "sc", "stop", "SonarQube"};
+			    // check
+			    //String[] script = {"cmd.exe", "/c", "sc", "query", "SonarQube", "|", "find", "/C", "\"RUNNING\""};
+			    
+			    Process process = Runtime.getRuntime().exec(script);
+			    
+			    StringWriter writer = new StringWriter();
+			    IOUtils.copy(process.getInputStream(), writer, "UTF-8");
+			    System.out.println(writer.toString());
+			    System.out.println(process.exitValue());
+			
+//			String path = "C:/sonarqube-6.7.2/bin/windows-x86-64/StartSonar.bat";
+////			CommandLine commandLine = CommandLine.parse(path);
+////			DefaultExecutor executor = new DefaultExecutor();
+////			executor.setExitValue(1);
+//			try {
+//				System.out.println("I'm trying to start "+path);
+//				Runtime.getRuntime().exec("cmd /c start "+path);
+////				int exitValue = executor.execute(commandLine);
+////				System.out.println("Executed. Exit value: "+exitValue);
 			} catch (ExecuteException e) {
 				System.out.println("Execute Exception");
 			} catch (IOException e) {
